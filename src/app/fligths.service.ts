71309548b242c9f69d38ecd21b08d4b95eb3943e
vinigonz1993 <vinigonzalez1993@gmail.com>
2023-03-23 19:13:26 -0400

    Creating requestConfig file for request simple vars

--- src/app/fligths.service.ts
+++ src/app/fligths.service.ts
@@ -2,23 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from './models/flight.model';
+import { BASE_URL, HEADERS, RESOURCES } from './requestConfig';
 
 @Injectable({
     providedIn: 'root',
 })
 export class FlightService {
-    baseUrl = 'http://xiapp.tradewindapps.com:9912/currentflights';
-
     constructor(private http: HttpClient) {};
 
     public get(): Observable<Flight[]> {
         return this.http.get<Flight[]>(
-            this.baseUrl,
+            `${BASE_URL}/${RESOURCES.currentflights}`,
             {
-                headers: {
-                    contentType: 'application/json',
-                    'X-API-Token': '9265ee96af02fe95e132e33a447f8f08',
-                },
+                headers: HEADERS
             },
         )
     };
---
+++ src/app/requestConfig.ts
@@ -0,0 +1,10 @@
+export const HEADERS = {
+    contentType: 'application/json',
+    'X-API-Token': '9265ee96af02fe95e132e33a447f8f08',
+};
+
+export const BASE_URL = 'http://xiapp.tradewindapps.com:9912/';
+export const RESOURCES = {
+    airports: '/airports',
+    currentflights: '/currentflights',
+}
